84deadd07764f6372cbfdcd80aa482f0d5351f8e
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-08-12 11:43:05 -0300

    getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,19 @@
 import Layout from '../components/Layout/Layout'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ countries }) {
+  console.log(countries);
+  
   return <Layout>main</Layout>;
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    }
+  }
 }
