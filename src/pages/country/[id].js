47f1627e6fb382379837435067a316f9ce075de8
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-08-14 15:03:45 -0300

    getServerSideProps (Server Side Rendering)

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,5 +1,21 @@
-const Country = () => {
+const Country = ({ country }) => {
+  console.log(country);
+  
   return <div>Country</div>
 }
 
 export default Country;
+
+export const getServerSideProps = async ({ params }) => {
+  const res = await fetch(
+    `https://restcountries.eu/rest/v2/alpha/${params.id}`
+  );
+  
+  const country = await res.json();
+
+  return {
+    props: {
+      country,
+    },
+  }
+}
