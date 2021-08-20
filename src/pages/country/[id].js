1786b6b859cd78824b252a99c00061c25abce5de
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-08-20 20:42:16 -0300

    getStaticProps and getStaticPaths

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -111,6 +111,31 @@ const Country = ({ country }) => {
 
 export default Country;
 
+export const getStaticPaths = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  const paths = countries.map(country => ({
+    params: { id: country.alpha3Code },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+  const country = await getCountry(params.id);
+
+  return {
+    props: {
+      country,
+    },
+  }
+}
+
+/*
 export const getServerSideProps = async ({ params }) => {
   const country = await getCountry(params.id);
 
@@ -120,3 +145,4 @@ export const getServerSideProps = async ({ params }) => {
     },
   }
 }
+*/
